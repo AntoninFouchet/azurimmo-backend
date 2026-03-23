393998c88182dd418dda3f9d6a72a08778ddc379
sio2 <antoninfouchet@gmail.com>
2026-03-23 12:00:59 +0100

    Ajout de DTO pour TypeIntervention

---
+++ src/main/java/bts/sio/azurimmo/dto/TypeInterventionDTO.java
@@ -0,0 +1,11 @@
+package bts.sio.azurimmo.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TypeInterventionDTO {
+    private Long id;
+    private String libelle;
+}
