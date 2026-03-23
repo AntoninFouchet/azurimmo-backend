e8e9ff52d0430026d50af9147bbed6369dc1b9b0
sio2 <antoninfouchet@gmail.com>
2026-03-23 12:00:20 +0100

    Ajout de repository pour TypeIntervention

---
+++ src/main/java/bts/sio/azurimmo/repository/TypeInterventionRepository.java
@@ -0,0 +1,9 @@
+package bts.sio.azurimmo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import bts.sio.azurimmo.model.TypeIntervention;
+
+@Repository
+public interface TypeInterventionRepository extends JpaRepository<TypeIntervention, Long> {
+}
