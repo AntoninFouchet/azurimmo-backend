93f3438d056ed21177cb25c0d5bbd9621a77ff9f
sio2 <antoninfouchet@gmail.com>
2026-03-23 11:56:54 +0100

    Ajout de controller pour Prestataire

---
+++ src/main/java/bts/sio/azurimmo/controller/PrestataireController.java
@@ -0,0 +1,39 @@
+package bts.sio.azurimmo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import bts.sio.azurimmo.dto.PrestataireDTO;
+import bts.sio.azurimmo.model.Prestataire;
+import bts.sio.azurimmo.service.PrestataireService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@RestController
+@RequestMapping("/api/prestataires")
+@CrossOrigin(origins = "http://localhost:5173")
+@Tag(name = "Prestataire", description = "API pour la gestion des prestataires")
+public class PrestataireController {
+
+    @Autowired
+    private PrestataireService prestataireService;
+
+    @PostMapping("/")
+    @Operation(summary = "Ajouter un prestataire")
+    public Prestataire createPrestataire(@RequestBody Prestataire prestataire) {
+        return prestataireService.savePrestataire(prestataire);
+    }
+
+    @GetMapping("/")
+    @Operation(summary = "Lister tous les prestataires")
+    public List<PrestataireDTO> getAllPrestataires() {
+        return prestataireService.getAllPrestataires();
+    }
+}
