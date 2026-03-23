c9bb622a5f4b1cc9f35711c121fdf81151e1bb5f
sio2 <antoninfouchet@gmail.com>
2026-03-23 13:17:17 +0100

    Ajout de controller pour Intervention

---
+++ src/main/java/bts/sio/azurimmo/controller/InterventionController.java
@@ -0,0 +1,39 @@
+package bts.sio.azurimmo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import bts.sio.azurimmo.dto.InterventionDTO;
+import bts.sio.azurimmo.model.Intervention;
+import bts.sio.azurimmo.service.InterventionService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@RestController
+@RequestMapping("/api/interventions")
+@CrossOrigin(origins = "http://localhost:5173")
+@Tag(name = "Intervention", description = "API pour la gestion des interventions")
+public class InterventionController {
+
+    @Autowired
+    private InterventionService interventionService;
+
+    @PostMapping("/")
+    @Operation(summary = "Ajouter une intervention")
+    public Intervention createIntervention(@RequestBody Intervention intervention) {
+        return interventionService.saveIntervention(intervention);
+    }
+
+    @GetMapping("/")
+    @Operation(summary = "Lister toutes les interventions")
+    public List<InterventionDTO> getAllInterventions() {
+        return interventionService.getAllInterventions();
+    }
+}
